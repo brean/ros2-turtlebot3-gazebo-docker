3b679e5618c282775ba0efc22b54b9691b9e825c
Andreas Bresser <andreas.bresser@dfki.de>
2023-01-19 19:20:35 +0100

    do not run rviz2, navigation already starts it

--- run.bash
+++ run.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 terminator -l turtlebot -g terminator.conf
 xhost -local:root
-cd remote_pc/ && docker-compose down
-cd simulated_turtlebot && docker-compose down
+cd remote_pc/ && docker-compose down && cd ..
+cd simulated_turtlebot && docker-compose down && cd ..
