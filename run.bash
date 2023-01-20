633f7c28a2d9dbb9533f4980647277c29a42bd48
Andreas Bresser <andreas.bresser@dfki.de>
2023-01-20 14:20:37 +0100

    run simulated turtlebot with external network

--- run.bash
+++ run.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
+docker network create turtlebot
 terminator -l turtlebot -g terminator.conf
 xhost -local:root
 cd remote_pc/ && docker-compose down && cd ..
 cd simulated_turtlebot && docker-compose down && cd ..
+docker network rm turtlebot
