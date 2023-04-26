423e55a161fba6663e8178bd2be79ec5eab5ca18
Andreas Bresser <andreas.bresser@dfki.de>
2023-04-26 14:06:29 +0200

    unify names, put gazebo-version in tag

--- simulated_turtlebot/build.bash
+++ simulated_turtlebot/build.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
-time docker build --build-arg ROS_DISTRO=foxy -t brean/ros2-turtlebot3-gazebo:foxy .
-time docker push brean/ros2-turtlebot3-gazebo:foxy
+time docker build --build-arg ROS_DISTRO=foxy -t brean/ros2-turtlebot3:foxy-classic .
+# time docker push brean/ros2-turtlebot3:foxy-classic
 # building takes around 5 minutes on an i7-8550U with fast internet
